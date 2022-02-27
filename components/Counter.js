da92601bc0a6b2a8271e9b74f0dead8157d2b8b8
nengcham <nengcham7@gmail.com>
2022-02-27 14:20:16 +0900

    lotto add

--- components/Counter.js
+++ components/Counter.js
@@ -3,9 +3,9 @@ import Layout from "../containers/Layout";
 export default function Counter(){
     const [count, setCount] = useState(0)
 
-    return(<Layout>
+    return<Layout>
         <button onClick={()=>{setCount(count + 1)}}>+</button><br />
         <button onClick={()=>{setCount(count - 1)}}>-</button>
         <div>{count}</div>
-        </Layout>)
+        </Layout>
 }
---
+++ components/Lotto.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Layout from "../containers/Layout";
+
+export default function Lotto(){
+    
+
+    return<Layout>
+    <h1>Lotto</h1>
+    <button>로또 번호받기</button>
+    <div></div>
+    </Layout>
+}
